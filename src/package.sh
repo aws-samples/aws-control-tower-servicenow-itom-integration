4fe3eb79abe05eed9e16f6895c336b6939559975
Ajay Mehta <mehtaaa@amazon.com>
2021-01-04 14:49:00 -0600

    Updated comments, removed unneeded files

--- src/package.sh
+++ src/package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
- 
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
 # Builds a lambda package from a single Python 3 module with pip dependencies.
 # This is a modified version of the AWS packaging instructions:
 # https://docs.aws.amazon.com/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html#python-package-dependencies
